e752cbee8bad5b622c752d88b1c14a9bff726166
Superman <namazbekzhan@gmail.com>
2025-01-30 19:08:51 +0500

    Add optimized Pydantic schemas for user, team, and project with best practices

--- connecto-backend/app/models/__init__.py
+++ connecto-backend/app/models/__init__.py
@@ -4,6 +4,7 @@ from .team import Team
 from .project import Project, project_skills_association
 from .skill import Skill
 from .article import Article, Comment, article_tags_association
+
 # и т.д...
 __all__ = (
   "Base",

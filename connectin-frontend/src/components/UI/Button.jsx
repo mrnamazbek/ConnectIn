5f6f10a69adb56ee1095c9d7df39787e149a23a5
Didar Auyesbay <didarmaratov04@gmail.com>
2025-04-11 18:17:24 +0500

    Fix front deploy error on railway

--- connectin-frontend/src/components/UI/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ children, className }) => {
-    return <button className={`p-3 bg-black text-white rounded-lg hover:bg-gray-800 transition-all ${className}`}>{children}</button>;
-};
-
-export default Button;
